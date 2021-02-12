8dae6b70cf5dc1d77f4b4b667f3216ce1e20e2a4
zoeyzsz <76697248+zoeyzsz@users.noreply.github.com>
2021-02-12 19:47:53 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,7 +13,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from bot.helper.telegram_helper.filters import CustomFilters
-from bot.modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, speedtest
+from bot.modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, speedtest, information
 
 
 @run_async
