9776d3a2d8b0f32cbaa8a88abcde40d34946a75a
zoeyzsz <76697248+zoeyzsz@users.noreply.github.com>
2021-02-12 17:56:41 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -8,7 +8,7 @@ class _BotCommands:
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
         self.SpeedCommand = 'speed'
-        self.YTSCommand = 'yts'
+        self.YtsCommand = 'yts'
         self.StatusCommand = 'status'
         self.AuthorizeCommand = 'auth'
         self.UnAuthorizeCommand = 'unauth'
