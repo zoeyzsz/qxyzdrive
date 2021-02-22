34577280eb5247e03baf6c368b9cdaa19306253d
zoeyzsz <zoeyzsz@noreply.github.com>
2021-02-22 23:12:04 +0700

    update

--- bot/__init__.py
+++ bot/__init__.py
@@ -87,7 +87,7 @@ try:
         DOWNLOAD_DIR = DOWNLOAD_DIR + '/'
     DOWNLOAD_STATUS_UPDATE_INTERVAL = int(getConfig('DOWNLOAD_STATUS_UPDATE_INTERVAL'))
     OWNER_ID = int(getConfig('OWNER_ID'))
-    OWNER_IDS = int(getConfig('OWNER_IDS'))
+    OWNER_IDS = tuple(filter(lambda x: x, map(int, os.environ.get("OWNER_IDS", "").split())))
     AUTO_DELETE_MESSAGE_DURATION = int(getConfig('AUTO_DELETE_MESSAGE_DURATION'))
     TELEGRAM_API = getConfig('TELEGRAM_API')
     TELEGRAM_HASH = getConfig('TELEGRAM_HASH')
