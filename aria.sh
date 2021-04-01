f7f76a6b4a5a48b32c4c2cb33216b52980ce83b6
zoeyzsz <76697248+zoeyzsz@users.noreply.github.com>
2021-04-01 23:31:09 +0700

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,20 @@
+file="trackers.txt"
+echo "$(curl -Ns https://trackerslist.com/all.txt | awk '$1' | tr ',' '\n')" > trackers.txt
+echo "$(curl -Ns https://raw.githubusercontent.com/zoeyzsz/trackerslist/master/trackers_all.txt)" >> trackers.txt
+tmp=$(sort trackers.txt | uniq) && echo "$tmp" > trackers.txt
+sed -i '/^$/d' trackers.txt
+sed -z -i 's/\n/,/g' trackers.txt
+tracker_list=$(cat trackers.txt)
+if [ -f $file ] ; then
+    rm $file
+fi
+tracker="[$tracker_list]"
 export MAX_DOWNLOAD_SPEED=0
 export MAX_CONCURRENT_DOWNLOADS=10
-aria2c --enable-rpc --enable-dht --rpc-listen-all=false --rpc-listen-port 6800 --dht-listen-port=6881 --check-certificate=false \
-   --max-connection-per-server=10 --rpc-max-request-size=1024M --max-tries=5 \
-   --seed-time=0.01 --seed-ratio=1.0 --min-split-size=10M --follow-torrent=mem --follow-metalink=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-download-limit=0 --max-overall-upload-limit=0 --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \
+  --max-connection-per-server=10 --rpc-max-request-size=1024M --max-tries=5 \
+  --bt-tracker=$tracker --bt-max-peers=0 --seed-time=0.01 --seed-ratio=1.0 \
+  --min-split-size=10M --follow-torrent=mem --follow-metalink=mem --split=10 \
+  --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+  --max-overall-download-limit=0 --max-overall-upload-limit=0 --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
+  --peer-id-prefix=-TR2770- --user-agent=Transmission/2.77 --peer-agent=Transmission/2.77
